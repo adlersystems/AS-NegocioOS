22c411ea576b7e5ee67893c314288dba71a9a41e
Rafael Aguilar <rafaguilarsol@gmail.com>
2026-09-06 10:02:41 -0600

    Add favicon and set default app name

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -31,6 +31,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('images/logo/logo-icon.svg') }}">
 
     <title>{{ $title }} — {{ $companyName }}</title>
 
